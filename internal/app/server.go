02f58db31891752d3ecde9bde5bb3c2cf2649cad
Vitaly <79vitaly79@gmail.com>
2019-10-02 19:40:11 +0300

    Added readme

--- internal/app/server.go
+++ internal/app/server.go
@@ -2,7 +2,6 @@ package server
 
 import (
 	"crypto/tls"
-	"flag"
 	"log"
 	"net/http"
 	"proxy/internal/pkg/db"
@@ -11,16 +10,7 @@ import (
 
 func StartApp(port string) {
 	db.Connect()
-	var pemPath string
-	flag.StringVar(&pemPath, "pem", "server.pem", "path to pem file")
-	var keyPath string
-	flag.StringVar(&keyPath, "key", "server.key", "path to key file")
-	var proto string
-	flag.StringVar(&proto, "proto", "http", "Proxy protocol (http or https)")
-	flag.Parse()
-	if proto != "http" && proto != "https" {
-		log.Fatal("Protocol must be either http or https")
-	}
+
 	server := &http.Server{
 		Addr: ":" + port,
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -30,13 +20,10 @@ func StartApp(port string) {
 				handlers.HandleHTTP(w, r)
 			}
 		}),
-		// Disable HTTP/2.
+
 		TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler)),
 	}
-	if proto == "http" {
-		log.Fatal(server.ListenAndServe())
-	} else {
-		log.Fatal(server.ListenAndServeTLS(pemPath, keyPath))
-	}
+
+	log.Fatal(server.ListenAndServe())
 
 }
